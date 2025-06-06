729ca78326984285642b64b1508844d67ebf45ba
Yaron Pri-Gal <89332819+yaronpri@users.noreply.github.com>
2025-06-06 20:39:25 +0300

    Committing 4 items from workspace e96e714d-8587-4d9c-9590-3bd01b298b28

---
+++ reprows/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "e3d073d9-899d-4e98-b2b0-140e4ca2decd",
+# META       "default_lakehouse_name": "lake1",
+# META       "default_lakehouse_workspace_id": "e96e714d-8587-4d9c-9590-3bd01b298b28",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "e3d073d9-899d-4e98-b2b0-140e4ca2decd"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+print(1)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
